17bd6f6a39202e79278022ed1434470ea8d52329
Mickaël <mickael.aubert63@gmail.com>
2020-12-05 19:56:54 +0100

    Création des vues xml pour la liste des biens immobliers

--- app/src/main/java/com/openclassrooms/realestatemanager/MainActivity.kt
+++ app/src/main/java/com/openclassrooms/realestatemanager/MainActivity.kt
@@ -10,23 +10,14 @@ Represents the main activity. This is the activity of launching the application.
  */
 class MainActivity : AppCompatActivity() {
 
+    /**
+     * Class used to bind XML views to code.
+     */
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         this.binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(this.binding.root)
-
-        configureTextViewMain()
-        configureTextViewQuantity()
-    }
-
-    private fun configureTextViewMain() {
-        this.binding.quantityLabel.text ="Le premier bien immobilier enregistré vaut "
-    }
-
-    private fun configureTextViewQuantity() {
-        val quantity = Utils.convertDollarToEuro(100)
-        this.binding.quantity.text = quantity.toString()
     }
 }
